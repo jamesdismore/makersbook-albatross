295c1a9b8b5ec4e3a16640dcaeb2af3b651f576e
James Dismore <jamesdismore@outlook.com>
2025-06-03 13:41:05 +0100

    changed_database_added_to_post_model

--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -3,6 +3,8 @@ package com.makersacademy.acebook.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 import static java.lang.Boolean.TRUE;
 
 @Data
@@ -14,13 +16,21 @@ public class User {
     private Long id;
     private String username;
     private boolean enabled;
+    private String avatar;
+    private LocalDate dob;
+    private String first_name;
+    private String last_name;
 
     public User() {
         this.enabled = TRUE;
     }
 
-    public User(String username) {
+    public User(String username,String first_name, String last_name, String avatar, LocalDate dob) {
         this.username = username;
+        this.avatar = avatar;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.dob = dob;
         this.enabled = TRUE;
     }
 
