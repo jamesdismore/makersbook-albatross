7aaf6c726bcfdbcff2c93d2fe18a434e1144eee7
James Dismore <jamesdismore@outlook.com>
2025-06-03 16:30:08 +0100

    added_all_repos

---
+++ src/main/java/com/makersacademy/acebook/repository/FriendshipRepository.java
@@ -0,0 +1,7 @@
+package com.makersacademy.acebook.repository;
+
+import com.makersacademy.acebook.model.Friendship;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FriendshipRepository extends CrudRepository<Friendship, Long> {
+}
---
+++ src/main/java/com/makersacademy/acebook/repository/PostLikeRepository.java
@@ -0,0 +1,7 @@
+package com.makersacademy.acebook.repository;
+
+import com.makersacademy.acebook.model.PostLike;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostLikeRepository extends CrudRepository<PostLike, Long> {
+}
