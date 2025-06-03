db4e01ff46de37c4c61cbd4e44d186aed2eed9c3
Maddy Miller <maddyjmiller@outlook.com>
2025-06-03 14:44:04 +0100

    screened users, new users now diverted to registration page

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -2,13 +2,19 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping
 public class UsersController {
     @Autowired
     UserRepository userRepository;
@@ -21,10 +27,42 @@ public class UsersController {
                 .getPrincipal();
 
         String username = (String) principal.getAttributes().get("email");
-        userRepository
-                .findUserByUsername(username)
-                .orElseGet(() -> userRepository.save(new User(username)));
+        Optional<User> user = userRepository.findUserByUsername(username);
+        if (user.isPresent()){
+            System.out.println(user.get().getId());
+            return new RedirectView("/posts");
+        } else {
+            return new RedirectView("/users/newUser");
+        }
+        // below -> previous method of returning posts w email welcome OR saving user and then logging in
+            //
+//        userRepository
+//                .findUserByUsername(username)
+//                .orElseGet(() -> userRepository.save(new User(username)));
+//
+//        return new RedirectView("/posts");
+    }
+    @GetMapping("/users/newUser")
+    public ModelAndView afterSignUp() {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
 
-        return new RedirectView("/posts");
+        // below - post mapping?
+        String username = (String) principal.getAttributes().get("email");
+        User user = new User(username);
+        ModelAndView modelAndView = new ModelAndView("NewUserPage", "our_user", user);
+        return modelAndView;
+    }
+    @PostMapping("/users/newUser")
+    public RedirectView saveNewUser(@Valid User user, BindingResult result) {
+        System.out.println("Check");
+        if (result.hasErrors()) { // errors come from class constraints
+            return new RedirectView("/users/newUser");
+        } else {
+            userRepository.save(user); // fill out parameters as database changes to include first name, last name, dob etc
+            return new RedirectView("/posts");
+        }
     }
 }
--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -1,23 +1,26 @@
 package com.makersacademy.acebook.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import static java.lang.Boolean.TRUE;
 
 @Data
 @Entity
+@Getter @Setter @NoArgsConstructor
+
 @Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank // means we don't need to specify elsewhere that it can't be blank
     private String username;
     private boolean enabled;
-
-    public User() {
-        this.enabled = TRUE;
-    }
 
     public User(String username) {
         this.username = username;
