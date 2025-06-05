f89d73e0b2302510fadcb0c180b30028e6aadc87
Maddy Miller <maddyjmiller@outlook.com>
2025-06-05 15:18:22 +0100

    user registration page working i think, but users have to be 14 or over or it breaks

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -9,9 +9,14 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Controller
@@ -49,12 +54,27 @@ public class UsersController {
 
     }
     @PostMapping("/users/newUser")
-    public String saveNewUser(@Valid @ModelAttribute("our_user") User user, BindingResult result) {
+    public String saveNewUser(@Valid @ModelAttribute("our_user") User user, @RequestParam("file") MultipartFile file, BindingResult result) {
         if (result.hasErrors()) { // error messages come from class constraints (needs dependency)
             return "newUser"; //stays on page
-        } else {
-            userRepository.save(user); // this saves user to database, eventually fill out parameters as database changes to include first name, last name, dob etc
-            return "redirect:/posts"; // redirects to posts
         }
+        userRepository.save(user);
+        // this saves user to database, eventually fill out parameters as database changes to include first name, last name, dob etc
+
+        if (!file.isEmpty()) {
+            try {
+                String uploadDir = "src/main/resources/static/images/userAvatars/";
+                String filename = user.getId() + ".jpg"; // save as {userId}.jpg
+                Path path = Paths.get(uploadDir + filename);
+                Files.write(path, file.getBytes());
+
+                // Set filename in the user object & update database
+                user.setAvatar(filename); // Store "9.jpg" in DB
+                userRepository.save(user);  // Save user with updated avatar path
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "redirect:/posts"; // redirects to posts
     }
 }
