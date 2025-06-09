31b417018d88a0f95610e334b1ff753986f29c31
James Dismore <jamesdismore@outlook.com>
2025-06-09 15:54:13 +0100

    fixed friend repo typo

--- src/main/java/com/makersacademy/acebook/repository/FriendRequestRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendRequestRepository.java
@@ -3,10 +3,9 @@ package com.makersacademy.acebook.repository;
 import com.makersacademy.acebook.model.FriendRequest;
 import org.springframework.data.repository.CrudRepository;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public interface FriendRequestRepository extends CrudRepository<FriendRequest, Long> {
     public ArrayList<FriendRequest> findFriendRequestByfromUserIdAndStatus(int fromUserId,String status);
-    public ArrayList<FriendRequest> findFriendRequestBytoUserIdAndStatus(int fromUserId,String status);
+    public ArrayList<FriendRequest> findFriendRequestBytoUserIdAndStatus(int toUserId, String status);
 }
