84740c2d88174324972497d4df438f67b2db992c
James Dismore <jamesdismore@outlook.com>
2025-06-09 16:26:27 +0100

    user search by like username first name or last name

--- src/main/java/com/makersacademy/acebook/controller/TestController.java
+++ src/main/java/com/makersacademy/acebook/controller/TestController.java
@@ -2,7 +2,9 @@ package com.makersacademy.acebook.controller;
 
 
 import com.makersacademy.acebook.model.FriendRequest;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.FriendRequestRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,9 @@ public class TestController {
     @Autowired
     FriendRequestRepository friendRequestRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/test")
     public String testpage(){
         ArrayList<FriendRequest> friendRequests = friendRequestRepository.findFriendRequestByfromUserIdAndStatus(7,"PENDING");
@@ -24,5 +29,11 @@ public class TestController {
     public String testpagependingto(){
         ArrayList<FriendRequest> friendRequests = friendRequestRepository.findFriendRequestBytoUserIdAndStatus(7,"PENDING");
         return friendRequests.toString();
+    }
+
+    @GetMapping("/testjsql")
+    public String testjsql(){
+        ArrayList<User> output = userRepository.testFunction("sa");
+        return output.toString();
     }
 }
--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     public Optional<User> findUserByUsername(String username);
+
+    @Query(value="SELECT * FROM users u WHERE u.username LIKE %?1% OR u.first_name LIKE %?1% OR u.last_name LIKE %?1%",nativeQuery = true)
+    ArrayList<User> testFunction(String input);
 }
