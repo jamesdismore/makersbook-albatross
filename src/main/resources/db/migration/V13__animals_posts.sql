18d9e732814ac31ed0c876a77222b4a839ec7d1f
Paul Patterson <pazerp@hotmail.com>
2025-06-13 13:05:00 +0100

    Sample posts from the animals.

---
+++ src/main/resources/db/migration/V13__animals_posts.sql
@@ -0,0 +1,20 @@
+-- Raven already friends with...
+INSERT INTO friendships(user_id, friend_id, friendship_timestamp) VALUES
+-- ...wolf
+(13, 8, '2024-05-25 13:00:00'),
+(8, 13, '2024-05-25 13:00:00'),
+
+-- ...bear
+(13, 10, '2024-05-27 14:00:00'),
+(10, 13, '2024-05-27 14:00:00');
+
+-- Two Raven posts
+INSERT INTO posts(content, user_id, post_timestamp) VALUES
+ (E'Greetings all. I am Raven, master of the morning breeze and shadow of the twilight. Let me share a glimpse into my daily life, a rhythm shaped by wings, wind, and wit.', 13, '2025-06-03 14:00:00'),
+ (E'I am the king of the savannah, and nothing stirs my spirit like the chase. From the moment I catch sight of my prey, a surge of power pulses through my muscles — sharp, focused, alive.', 14, '2025-06-04 15:00:00'),
+ (E'Hello from the treetops! I’m Raven, architect of the wild, and I’d like to share the story of how I build my nest — a sturdy sanctuary high above the world below.', 13, '2025-06-06 14:00:00'),
+ (E'Every day, I roam the forests and mountains, feeling the weight of fear and anger that shepherds carry against me. They see me as a threat, not as a creature just trying to survive like them.', 8, '2025-04-06 15:00:00'),
+ (E'I feel the cold breath of extinction closing in, as the lands I once ruled grow silent and empty. Every step I take is shadowed by hunters’ relentless pursuit, leaving fewer of my kind to roam free.', 8, '2024-06-07 15:00:00'),
+ (E'If there’s one thing I truly look forward to each year, it’s the long, deep sleep of hibernation. After months of foraging, and feeling the pulse of the wild, there’s nothing like curling up in my den.', 10, '2024-08-01 15:00:00');
+
+INSERT INTO users (username,dob,first_name,last_name,enabled,avatar) VALUES ('lion@ex.com','1980-01-01','Len','Lion','TRUE','14');
